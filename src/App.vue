0411e59161ffe63e38432e85f70cd161d8c53d80
raven <13001130055@163.com>
2020-05-21 16:46:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+
+    <el-container>
+      <el-header>
+        <Nav></Nav>
+      </el-header>
+      <el-container style="width:1100px;margin:0 auto;border:1px solid blue;">
+        <el-main>
+          <router-view />
+        </el-main>
+        <el-aside width="250px">Aside</el-aside>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0 auto;
+  padding: 0px;
+}
+
+.el-header {
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  box-shadow: 0 1px 3px rgba(26, 26, 26, 0.1);
+  background-clip: content-box;
+  margin-bottom: 20px;
+}
+
+.el-aside {
+  color: #333;
+
+  text-align: center;
+  line-height: 200px;
+  border: 1px solid pink;
+}
+
+.el-main {
+  border: 1px solid red;
+  padding: 0px;
+  margin-right: 20px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+  width: 1000px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <el-menu :default-active="activeIndex"
+           class="el-menu-demo"
+           mode="horizontal"
+           style="margin:0 auto;width:1100px;border:none;"
+           router
+           @select="handleSelect">
+
+    <el-menu-item>
+      <img src="@/assets/riveredlogo.png"
+           height="35px;"
+           style="text-align: center;"
+           alt="">
+    </el-menu-item>
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-menu-item index="/photo">摄影</el-menu-item>
+    <el-submenu index="3">
+      <template slot="title">语言</template>
+      <el-menu-item index="3-1">php</el-menu-item>
+      <el-menu-item index="3-2">Laravel</el-menu-item>
+      <el-menu-item index="3-3">ThinkPhp</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="/about">关于</el-menu-item>
+    <!-- <el-menu-item index="4"
+                  disabled>消息中心</el-menu-item> -->
+    <el-menu-item index="5"
+                  style="float:right;">登陆/注册</el-menu-item>
+  </el-menu>
+
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    };
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
